e790407f8ff630b1c0914105c4ccd04b93757a3b
hbc <bcxxxxxx@gmail.com>
2016-09-11 11:59:05 +0800

    feat(webhook): implement incoming webhook client

---
+++ webhook_client.go
@@ -0,0 +1,78 @@
+package bearychat
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type WebhookResponse struct {
+	StatusCode int
+	Code       int              `json:"code"`
+	Error      string           `json:"error",omitempty`
+	Result     *json.RawMessage `json:"result"`
+}
+
+func (w WebhookResponse) IsOk() bool {
+	return w.Code == 0
+}
+
+type WebhookClient interface {
+	// Set webhook webhook.
+	SetWebhook(webhook string) WebhookClient
+
+	// Set http client.
+	SetHTTPClient(client *http.Client) WebhookClient
+
+	// Send webhook payload.
+	Send(payload io.Reader) (*WebhookResponse, error)
+}
+
+type webhookClient struct {
+	httpClient *http.Client
+
+	Webhook string
+}
+
+func NewIncomingWebhookClient(webhook string) *webhookClient {
+	return &webhookClient{
+		httpClient: http.DefaultClient,
+
+		Webhook: webhook,
+	}
+}
+
+func (w *webhookClient) SetWebhook(webhook string) WebhookClient {
+	w.Webhook = webhook
+	return w
+}
+
+func (w *webhookClient) SetHTTPClient(c *http.Client) WebhookClient {
+	w.httpClient = c
+	return w
+}
+
+func (w *webhookClient) Send(payload io.Reader) (*WebhookResponse, error) {
+	if w.Webhook == "" {
+		return nil, errors.New("webhook url is required")
+	}
+	if w.httpClient == nil {
+		return nil, errors.New("http client is required")
+	}
+
+	resp, err := w.httpClient.Post(w.Webhook, "application/json", payload)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	webhookResponse := new(WebhookResponse)
+	webhookResponse.StatusCode = resp.StatusCode
+	if err := json.NewDecoder(resp.Body).Decode(webhookResponse); err != nil {
+		return nil, err
+	}
+
+	return webhookResponse, nil
+}
---
+++ webhook_client_test.go
@@ -0,0 +1,80 @@
+package bearychat
+
+import (
+	"net/http"
+	"testing"
+)
+
+const (
+	testWebhook = "http://localhost:3927"
+)
+
+func TestWebhookResponse_IsOk(t *testing.T) {
+	var resp WebhookResponse
+
+	resp = WebhookResponse{Code: 0}
+	if !resp.IsOk() {
+		t.Errorf("response should be ok when code is 0")
+	}
+
+	resp = WebhookResponse{Code: 1}
+	if resp.IsOk() {
+		t.Errorf("response should not be ok when code is not 0")
+	}
+}
+
+func TestIncomingWebhookClient_SetWebhook(t *testing.T) {
+	h := NewIncomingWebhookClient("")
+	if h.SetWebhook(testWebhook) == nil {
+		t.Errorf("should return webhook client")
+	}
+
+	if h.Webhook != testWebhook {
+		t.Errorf("should set webhook")
+	}
+}
+
+func TestIncomingWebhookClient_SetHTTPClient(t *testing.T) {
+	h := NewIncomingWebhookClient(testWebhook)
+
+	if h.httpClient != http.DefaultClient {
+		t.Errorf("should use `http.DefaultClient` by default")
+	}
+
+	testHTTPClient := &http.Client{}
+	if h.SetHTTPClient(testHTTPClient) == nil {
+		t.Errorf("should return webhook client")
+	}
+
+	if h.httpClient != testHTTPClient {
+		t.Errorf("should set http client")
+	}
+}
+
+func TestIncomingWebhookClient_Send_WithoutWebhook(t *testing.T) {
+	h := NewIncomingWebhookClient("")
+	_, err := h.Send(nil)
+	if err == nil {
+		t.Errorf("should not send when webhook is not set")
+	}
+}
+
+func TestIncomingWebhookClient_Send_WithoutHTTPClient(t *testing.T) {
+	h := NewIncomingWebhookClient(testWebhook)
+	h.SetHTTPClient(nil)
+	_, err := h.Send(nil)
+	if err == nil {
+		t.Errorf("should not send when http client is not set")
+	}
+}
+
+func ExampleIncomingWebhookClient() {
+	m := Incoming{Text: "Hello, BearyChat"}
+	payload, _ := m.Build()
+	resp, _ := NewIncomingWebhookClient("YOUR WEBHOOK URL").Send(payload)
+	if resp.IsOk() {
+		// parse resp result
+	} else {
+		// parse resp error
+	}
+}
