767e41b99d447e6f1c5bb79aed2c212770e3818c
hbc <bcxxxxxx@gmail.com>
2017-03-31 15:58:10 +0800

    feat(openapi): implement `message.info`

--- openapi/message.go
+++ openapi/message.go
@@ -1,5 +1,11 @@
 package openapi
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
 type MessageKey string
 
 type MessageSubtype string
@@ -38,3 +44,24 @@ type Message struct {
 }
 
 type MessageService service
+
+type MessageInfoOptions struct {
+	VChannelID string
+	Key        string
+}
+
+// Info implements `GET /message.info`
+func (m *MessageService) Info(ctx context.Context, opt *MessageInfoOptions) (*Message, *http.Response, error) {
+	endpoint := fmt.Sprintf("message.info?vchannel_id=%s&message_key=%s", opt.VChannelID, opt.Key)
+	req, err := m.client.newRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var message Message
+	resp, err := m.client.do(ctx, req, &message)
+	if err != nil {
+		return nil, resp, err
+	}
+	return &message, resp, nil
+}
